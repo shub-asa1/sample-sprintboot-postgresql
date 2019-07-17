7e346f1234791ffb5c0847eb2d9ab56dbc3762d1
Shubham Asati <shubham.asati@ad.infosys.com>
2019-07-17 18:20:12 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		 try (Connection conn = DriverManager.getConnection(
+	                "jdbc:postgresql://edb-service/edb", "dbuser", "dbpwd")) {
+
+	            if (conn != null) {
+	                System.out.println("Connected to the database!");
+	            } else {
+	                System.out.println("Failed to make connection!");
+	            }
+
+	        } catch (SQLException e) {
+	            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+
+	    }
+	}
+
+
